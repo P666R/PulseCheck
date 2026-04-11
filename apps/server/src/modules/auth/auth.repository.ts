12929758357ce3a077b9557064fbf9b705993ec8
P666R <sam469roger@gmail.com>
2026-04-11 11:55:54 +0530

    feat(server): ✨ implement auth repository

---
+++ apps/server/src/modules/auth/auth.repository.ts
@@ -0,0 +1,99 @@
+import type { Prisma } from '@repo/db';
+
+import { db } from '#src/lib/prisma/client.prisma.js';
+
+export class AuthRepository {
+  private readonly prisma = db.getClient();
+  private readonly defaultOmit = {
+    password: true,
+    refreshTokens: true,
+  } as const;
+
+  async register(data: Prisma.UserCreateInput) {
+    return this.prisma.user.create({
+      data,
+      omit: this.defaultOmit,
+    });
+  }
+
+  async findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+      omit: this.defaultOmit,
+    });
+  }
+
+  async findByEmailInsecure(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+
+  async findById(id: string) {
+    return this.prisma.user.findUnique({
+      where: { id },
+      omit: this.defaultOmit,
+    });
+  }
+
+  async updateRefreshTokens(id: string, tokens: string[]) {
+    return this.prisma.user.update({
+      where: { id },
+      data: { refreshTokens: tokens },
+      omit: this.defaultOmit,
+    });
+  }
+
+  async updatePassword(id: string, passwordHash: string) {
+    return this.prisma.user.update({
+      where: { id },
+      data: {
+        password: passwordHash,
+        passwordChangedAt: new Date(),
+        refreshTokens: { set: [] },
+      },
+      omit: this.defaultOmit,
+    });
+  }
+
+  async findByRefTokenAndDeleteRefToken(refreshToken: string) {
+    return this.prisma.$transaction(async (tx) => {
+      const user = await tx.user.findFirst({
+        where: { refreshTokens: { has: refreshToken } },
+      });
+
+      if (!user) return null;
+
+      const updatedTokens = user.refreshTokens.filter(
+        (rt) => rt !== refreshToken,
+      );
+
+      return tx.user.update({
+        where: { id: user.id },
+        data: { refreshTokens: { set: updatedTokens } },
+        omit: this.defaultOmit,
+      });
+    });
+  }
+
+  async findByRefTokenAndRotateRefToken(oldToken: string, newToken: string) {
+    return this.prisma.$transaction(async (tx) => {
+      const user = await tx.user.findFirst({
+        where: { refreshTokens: { has: oldToken } },
+      });
+
+      if (!user) return null;
+
+      const updatedTokens = [
+        ...user.refreshTokens.filter((token) => token !== oldToken),
+        newToken,
+      ].slice(-5);
+
+      return tx.user.update({
+        where: { id: user.id },
+        data: { refreshTokens: { set: updatedTokens } },
+        omit: this.defaultOmit,
+      });
+    });
+  }
+}
