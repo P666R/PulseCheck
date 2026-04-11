2d986ce68037542ee99d994439b30981a7d926bf
P666R <sam469roger@gmail.com>
2026-04-11 11:48:51 +0530

    feat(server): ✨ implement password strength using zxcvbn

---
+++ apps/server/src/lib/utils/password-strength.util.ts
@@ -0,0 +1,19 @@
+import { zxcvbn, zxcvbnOptions } from '@zxcvbn-ts/core';
+import * as zxcvbnCommonPackage from '@zxcvbn-ts/language-common';
+import * as zxcvbnEnPackage from '@zxcvbn-ts/language-en';
+
+const options = {
+  translations: zxcvbnEnPackage.translations,
+  graphs: zxcvbnCommonPackage.adjacencyGraphs,
+  dictionary: {
+    ...zxcvbnCommonPackage.dictionary,
+    ...zxcvbnEnPackage.dictionary,
+  },
+};
+
+zxcvbnOptions.setOptions(options);
+
+export const passwordStrength = (password: string) => {
+  const result = zxcvbn(password);
+  return result.score >= 3;
+};
