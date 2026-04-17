586aa3ce53b28e31ebaf4f6eea40c9849facf63c
P666R <sam469roger@gmail.com>
2026-04-17 09:23:10 +0530

    feat(server): ✨ implement users controller

---
+++ apps/server/src/modules/users/users.controller.ts
@@ -0,0 +1,92 @@
+import type { Request, Response } from 'express';
+
+import { StatusCodes } from 'http-status-codes';
+
+import type { AuthenticatedRequest } from '#src/modules/auth/auth.type.js';
+import type {
+  CreateUserInput,
+  UserIdInput,
+  UserQueryInput,
+} from '#src/modules/users/users.schema.js';
+
+import { BaseController } from '#src/modules/core/base.controller.js';
+import { UserService } from '#src/modules/users/users.service.js';
+
+export class UserController extends BaseController {
+  constructor(private readonly userService: UserService) {
+    super();
+    this.getProfile = this.getProfile.bind(this);
+    this.getAllUsers = this.getAllUsers.bind(this);
+    this.createUser = this.createUser.bind(this);
+    this.getUserDetails = this.getUserDetails.bind(this);
+    this.getSystemStats = this.getSystemStats.bind(this);
+  }
+
+  async getProfile(req: Request, res: Response) {
+    const { user } = req as AuthenticatedRequest;
+    const result = await this.userService.getProfile(user.id);
+
+    req.log.info('User profile fetched successfully');
+
+    return this.sendSuccessResponse(req, res, {
+      message: 'User profile fetched successfully',
+      data: result,
+    });
+  }
+
+  async getAllUsers(req: Request, res: Response) {
+    const query = req.query as unknown as UserQueryInput;
+
+    const result = await this.userService.getAllUsers(query);
+
+    req.log.info('Users fetched successfully');
+
+    return this.sendSuccessResponse(req, res, {
+      message: 'Users fetched successfully',
+      data: result,
+    });
+  }
+
+  async createUser(req: Request, res: Response) {
+    const { name, email, address, password, role } =
+      req.body as CreateUserInput;
+    const user = await this.userService.createUser({
+      name,
+      email,
+      address,
+      password,
+      role,
+    });
+
+    req.log.info({ newUserId: user.id }, 'User created successfully');
+
+    return this.sendSuccessResponse(req, res, {
+      message: 'User created successfully',
+      data: user,
+      status: StatusCodes.CREATED,
+    });
+  }
+
+  async getUserDetails(req: Request, res: Response) {
+    const { id } = req.params as UserIdInput;
+    const user = await this.userService.getUserDetails(id);
+
+    req.log.info('User details fetched successfully');
+
+    return this.sendSuccessResponse(req, res, {
+      message: 'User details fetched successfully',
+      data: user,
+    });
+  }
+
+  async getSystemStats(req: Request, res: Response) {
+    const stats = await this.userService.getSystemStats();
+
+    req.log.info('System stats fetched successfully');
+
+    return this.sendSuccessResponse(req, res, {
+      message: 'System stats fetched successfully',
+      data: stats,
+    });
+  }
+}
