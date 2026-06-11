1bd872bf6de4135ae0e4da294162edbadfb3aa0a
P666R <sam469roger@gmail.com>
2026-06-11 19:00:47 +0530

    fix(server): 🐛 remove redundant captureStackTrace

--- apps/server/src/lib/api/app-error.api.ts
+++ apps/server/src/lib/api/app-error.api.ts
@@ -17,18 +17,20 @@ export class AppError extends Error {
     details?: AppErrorDetails,
     options?: ErrorOptions,
   ) {
+    // 1. Pass message and options (like 'cause') to base Error class
     super(message, options);
+    // 2. Set the prototype explicitly
     Object.setPrototypeOf(this, new.target.prototype);
 
     this.name = this.constructor.name;
     this.statusCode = statusCode;
+    // 3. Logic for 'warn' (4xx) vs 'error' (5xx), for logs and monitoring only, not in responses
     this.status =
-      statusCode >= 400 && statusCode < 500 ? STATUS.FAIL : STATUS.ERROR;
-    this.isOperational = true;
+      statusCode >= 400 && statusCode < 500 ? STATUS.WARN : STATUS.ERROR;
+    // 4. Mark as operational (trusted error we know how to handle)
+    this.isOperational = statusCode < 500;
     this.errorCode = errorCode ?? this.name;
     this.details = details;
     this.timestamp = new Date().toISOString();
-
-    Error.captureStackTrace(this, this.constructor);
   }
 }
